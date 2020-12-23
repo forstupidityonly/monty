bffc68306252e4734a1e78bf88be6f0f053a4a4f
Corbin <forstupidityonly@gmail.com>
2020-12-22 18:11:10 -0600

    pchar betty

--- pchar.c
+++ pchar.c
@@ -7,17 +7,17 @@
  */
 void pchar(stack_t **stack, unsigned int line_count)
 {
-    if (!(*stack))
-    {
-        dprintf(STDERR_FILENO, "L%u: can't pchar, stack empty\n", line_count);
-        free_stuf();
-        exit(EXIT_FAILURE);
-    }
-    if (!isascii((*stack)->n))
-    {
-        dprintf(STDERR_FILENO, "L%u: can't pchar, value out of range\n", line_count);
-        free_stuf();
-        exit(EXIT_FAILURE);
-    }
-    printf("%c\n", (*stack)->n);
+	if (!(*stack))
+	{
+		dprintf(STDERR_FILENO, "L%u: can't pchar, stack empty\n", line_count);
+		free_stuf();
+		exit(EXIT_FAILURE);
+	}
+	if (!isascii((*stack)->n))
+	{
+		dprintf(STDERR_FILENO, "L%u: can't pchar, value out of range\n", line_count);
+		free_stuf();
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", (*stack)->n);
 }
